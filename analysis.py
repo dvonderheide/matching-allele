985ed8d75c2a6b50d523443591b40deaae8d1a3b
David Vonderheide <david.s.vonderheide.21@dartmouth.edu>
2021-01-10 16:55:48 -0500

    analysis

--- analysis.py
+++ analysis.py
@@ -4,10 +4,10 @@ def externalinfluence():
     pg = PG()
     pg.default("general:runtime", 20)
     
-    pg.default('species2:genotype', [(1,1),(1,0),(0,1),(0,0)])
-    pg.default('species1:genotype', [(1,1),(1,0),(0,1),(0,0)])
-    pg.default('species2:mut_rate', [0, 10e-6, 10e-2,1])  
-    pg.default('species1:mut_rate', [0, 10e-6, 10e-2,1])
+    pg.add('geno1','species2:genotype', [(1,1),(1,0),(0,1),(0,0)])
+    pg.add('geno2','species1:genotype', [(1,1),(1,0),(0,1),(0,0)])
+    pg.add('mut_rate1','species2:mut_rate', [0, 10e-6, 10e-2,1])  
+    pg.add('mut_rate2','species1:mut_rate', [0, 10e-6, 10e-2,1])
 
 
     pg.set_repeats(10)
