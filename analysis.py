15f3be98246e90ed84707ed8b3a812368a3f5c64
David Vonderheide <david.s.vonderheide.21@dartmouth.edu>
2021-01-10 17:01:17 -0500

    hopefully proper analysis

--- analysis.py
+++ analysis.py
@@ -2,12 +2,15 @@ from make_parameters import Paramgroup as PG
 import numpy as np
 def externalinfluence():
     pg = PG()
-    pg.default("general:runtime", 20)
+    pg.default("species2:genotype", [(1,1), (0,0)])
+    pg.default("species1:genotype", [(1,1), (0,0)])
+    pg.default("species2:mut_rate", 10e-6)
+    pg.default("species1:mut_rate", 10e-6)
     
-    pg.add('geno1','species2:genotype', [(1,1),(1,0),(0,1),(0,0)])
-    pg.add('geno2','species1:genotype', [(1,1),(1,0),(0,1),(0,0)])
-    pg.add('mut_rate1','species2:mut_rate', [0, 10e-6, 10e-2,1])  
-    pg.add('mut_rate2','species1:mut_rate', [0, 10e-6, 10e-2,1])
+    pg.add('test_geno','species2:genotype', [(1,1),(1,0),(0,1),(0,0)])
+    pg.add('test_geno','species1:genotype', [(1,1),(1,0),(0,1),(0,0)])
+    pg.add('test_mutrat','species2:mut_rate', [0, 10e-6, 10e-2,1])  
+    pg.add('test_mutrat','species1:mut_rate', [0, 10e-6, 10e-2,1])
 
 
     pg.set_repeats(10)
